86069ed05f50ee088ce595e4ed076c4afe816bca
Robert De La Torre <r.delatorre@outlook.com>
2023-10-15 22:32:27 -0700

    Adding my Main and Binary Tree Class.

---
+++ HomeworkFour/src/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+	
+	// DATA ATTRIBUTES
+	public int data;
+	public Node left, right;
+	
+	
+	// CONSTRUCTOR
+	public Node(int n) {
+		this.data = n;
+		left = right = null;
+	}
+	
+}
