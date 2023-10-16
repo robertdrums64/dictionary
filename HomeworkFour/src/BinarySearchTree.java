815525dee8af8d87ec64a7b49d141aa076fd10bb
Robert De La Torre <r.delatorre@outlook.com>
2023-10-15 22:34:46 -0700

    Adding my Main and Binary Tree Class.

---
+++ HomeworkFour/src/BinarySearchTree.java
@@ -0,0 +1,46 @@
+
+public class BinarySearchTree {
+
+	//MY ROOT
+	Node root;
+	
+	// INSERTION METHOD
+	public void insert(Node node) {
+		
+	}
+	// HELPER FOR THE INSERTION METHOD
+	private Node insertHelp(Node root, Node node) {
+		return null;
+	}
+	
+	//DISPLAY METHOD
+	public void display() {
+		
+	}
+	private void displayHelp(Node root) {
+		
+	}
+	
+	// SEARTH METHOD
+	public boolean search(int data) {
+		return false;
+	}
+	private boolean searchHelp(Node root, int data) {
+		return false;
+	}
+	
+	// REMOVE METHOD
+	public void remove(int data) {
+		
+	}
+	public Node removeHelp(Node root, int data) {
+		return null;
+	}
+	private int successor(Node root) {
+		return 0;
+	}
+	private int presuccessor(Node root) {
+		return 0;
+	}
+	
+}
