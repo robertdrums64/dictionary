af7853649f80627258f92e97eadf5c358bf78ad4
Robert De La Torre <r.delatorre@outlook.com>
2023-10-22 00:56:37 -0700

    Completed all methods. The application needs to be converted for String.

--- HomeworkFour/src/BinarySearchTree.java
+++ HomeworkFour/src/BinarySearchTree.java
@@ -6,41 +6,111 @@ public class BinarySearchTree {
 	
 	// INSERTION METHOD
 	public void insert(Node node) {
-		
+		root = insertHelp(root, node);
 	}
-	// HELPER FOR THE INSERTION METHOD
+	// HELPER FOR THE INSERTION METHOD (Helps with recursion)
 	private Node insertHelp(Node root, Node node) {
-		return null;
+		int data = node.data;
+		
+		if(root == null) {
+			root = node;
+			return root;
+		}
+		else if(data < root.data) {
+			root.left = insertHelp(root.left, node);
+		}
+		else {
+			root.right = insertHelp(root.right, node);
+		}
+		
+		return root;
 	}
 	
 	//DISPLAY METHOD
 	public void display() {
-		
+		displayHelp(root);
 	}
 	private void displayHelp(Node root) {
 		
+		if(root != null) {
+			displayHelp(root.left);
+			System.out.println(root.data);
+			displayHelp(root.right);
+		}
 	}
 	
 	// SEARTH METHOD
 	public boolean search(int data) {
-		return false;
+		return searchHelp(root, data);
 	}
 	private boolean searchHelp(Node root, int data) {
-		return false;
+		
+		if(root == null) {
+			return false;
+		}
+		else if(root.data == data) {
+			return true;
+		}
+		else if(root.data > data) {
+			return searchHelp(root.left, data);
+		}
+		else{
+			return searchHelp(root.right, data);
+		}
 	}
 	
 	// REMOVE METHOD
 	public void remove(int data) {
 		
+		if(search(data)) {
+			removeHelp(root, data);
+		}
+		else {
+			System.out.println(data + " could not be found");
+		}
+		
 	}
+	
 	public Node removeHelp(Node root, int data) {
-		return null;
+		
+		if(root == null) {
+			return root;
+		}
+		else if(data < root.data) {
+			root.left = removeHelp(root.left, data);
+		}
+		else if(data > root.data) {
+			root.right = removeHelp(root.right, data);
+		}
+		else {
+			if(root.left == null && root.right == null) {
+				root = null;
+			}
+			else if(root.right != null) {// FIND SUCCESSOR
+				root.data = successor(root);
+				root.right = removeHelp(root.right, root.data);
+			}
+			else { // FINDS A PREDECESSOR
+				root.data = successor(root);
+				root.left = removeHelp(root.left, root.data);
+			}
+		}
+		
+		return root;
 	}
 	private int successor(Node root) {
-		return 0;
+		root = root.right;
+		while(root.left != null) {
+			root = root.right;
+		}
+		return root.data;
 	}
 	private int presuccessor(Node root) {
-		return 0;
+		root = root.left;
+		while(root.right != null) {
+			root = root.right;
+		}
+		return root.data;
 	}
 	
 }
--- HomeworkFour/src/Main.java
+++ HomeworkFour/src/Main.java
@@ -5,6 +5,26 @@ public class Main {
 		
 		BinarySearchTree mytree = new BinarySearchTree();
 
+		mytree.insert(new Node(2));
+		mytree.insert(new Node(3));
+		mytree.insert(new Node(9));
+		mytree.insert(new Node(1));
+		mytree.insert(new Node(6));
+		mytree.insert(new Node(13));
+		mytree.insert(new Node(7));
+		mytree.insert(new Node(33));
+		mytree.insert(new Node(11));
+		mytree.insert(new Node(12));
+		mytree.insert(new Node(4));
+		mytree.insert(new Node(17));
+		mytree.insert(new Node(15));
+		
+		
+		mytree.display();
+		
+		System.out.println(mytree.search(1));
+		
+//		mytree.remove(1);
 	}
 
 }
